1d5af86b71a79fa77a7853f529618253c7a4b2a5
Alessandro LAMPARELLI <alessandro@lamparelli.eu>
2024-09-13 08:09:54 +0200

    started to works on folder setup for the boilerplate-project

---
+++ eslint.config.mjs
@@ -0,0 +1,31 @@
+// eslint.config.js
+import js from '@eslint/js';
+import perfectionistAlphabetical from 'eslint-plugin-perfectionist/configs/recommended-alphabetical';
+import reactPlugin from 'eslint-plugin-react';
+import reactHooksPlugin from 'eslint-plugin-react-hooks';
+import { fixupPluginRules } from '@eslint/compat';
+
+export default [
+	perfectionistAlphabetical,
+	js.configs.recommended,
+	{
+		plugins: {
+			// https://github.com/facebook/react/issues/28313
+			['react-hooks']: fixupPluginRules(reactHooksPlugin),
+			// https://github.com/jsx-eslint/eslint-plugin-react/issues/3699
+			['react']: fixupPluginRules(reactPlugin),
+		},
+		rules: {
+			'accessor-pairs': 'error',
+			'dot-notation': 'error',
+			eqeqeq: 'error',
+			'no-await-in-loop': 'error',
+			'no-constructor-return': 'error',
+			'no-undef': 'off',
+			'no-unused-vars': 'off',
+			'no-useless-return': 'error',
+			'no-var': 'error',
+			'sort-imports': 'error',
+		},
+	},
+];
