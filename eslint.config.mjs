06ab6205887b1f05bd3dbed35dccdc0d45427f62
Alessandro LAMPARELLI <alessandro@lamparelli.eu>
2024-09-16 07:21:46 +0200

    Removed Coffee : as a decision because there are aloready some big challenges and i don't want to be disgusted after 6 months.
    There are already some challenges here:
    - dynamic import, dynamic building blocks.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,12 @@
 // eslint.config.js
 import js from '@eslint/js';
 import perfectionistAlphabetical from 'eslint-plugin-perfectionist/configs/recommended-alphabetical';
-import reactPlugin from 'eslint-plugin-react';
-import reactHooksPlugin from 'eslint-plugin-react-hooks';
-import { fixupPluginRules } from '@eslint/compat';
-import coffee from 'eslint-plugin-coffee';
 
 export default [
 	perfectionistAlphabetical,
 	js.configs.recommended,
 	{
-		plugins: {
-			// https://github.com/facebook/react/issues/28313
-			['react-hooks']: fixupPluginRules(reactHooksPlugin),
-			// https://github.com/jsx-eslint/eslint-plugin-react/issues/3699
-			['react']: fixupPluginRules(reactPlugin),
-		},
+		plugins: {},
 		rules: {
 			'accessor-pairs': 'error',
 			'dot-notation': 'error',
